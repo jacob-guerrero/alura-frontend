a56171a11ebb728edcbb8af9e4e9aa153288eba3
Jacob <104036951+j31c0@users.noreply.github.com>
2023-08-02 11:09:03 -0500

    Agregando y eliminando elementos de listas

---
+++ js-bases/07-Listas.js
@@ -0,0 +1,28 @@
+const ciudad1 = "Bogota";
+const ciudad2 = "Lima";
+const ciudad3 = "Santiago";
+
+//Definicion de una lista con tipo de datos alfanumerico
+const ciudadesDisponibles = new Array("Bogota", "Lima", "Santiago");
+console.log(ciudadesDisponibles);
+
+//Definicion de una lista de forma abreviada
+const paisesDisponibles = ["Colombia", "Peru", "Chile", "Brasil", "Argentina"];
+console.log(paisesDisponibles);
+
+{
+    paisesDisponibles.push("Uruguay");
+    ciudadesDisponibles.push("Montevideo");
+
+    paisesDisponibles.unshift("Ecuador");
+    ciudadesDisponibles.unshift("Quito");
+    console.log(ciudadesDisponibles);
+    console.log(paisesDisponibles);
+}
+
+//Mostrando primer elemento de la lista
+console.log(paisesDisponibles[0]);
+console.log(ciudadesDisponibles[0]);
+
+paisesDisponibles.splice(1,2,'Venezuela');
+console.log(paisesDisponibles)
