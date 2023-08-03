09234cba32ffaf6eef72e424ed3aaf2ee2afec3e
Jacob <104036951+j31c0@users.noreply.github.com>
2023-08-03 14:50:52 -0500

    Agregar manejo de errores

--- js-formularios/js/validaciones.js
+++ js-formularios/js/validaciones.js
@@ -3,10 +3,57 @@ export function valida(input) {
     if (validadores[tipoDeInput]) {
         validadores[tipoDeInput](input);
     }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+]
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacio"
+    },
+    email: {
+        valueMissing: "El campo email no puede estar vacio",
+        typeMismatch: "El correo no es valido"
+    },
+    password: {
+        valueMissing: "El campo contraseña no puede estar vacio",
+        patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales."
+    },
+    nacimiento: {
+        valueMissing: "Este campo no puede estar vacio",
+        customError: "Debes tener al menos 18 años de edad"
+    }
 }
 
 const validadores = {
     nacimiento: (input) => validarNacimiento(input),
+}
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach( (error) => {
+        if(input.validity[error]) {
+            console.log(error);
+            console.log(input.validity[error]);
+            console.log(mensajesDeError[tipoDeInput][error]);
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    })
+
+    return mensaje
 }
 
 function validarNacimiento(input) {
