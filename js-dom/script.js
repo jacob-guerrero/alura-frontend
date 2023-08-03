40d53919a019b40746a46f45a0b90c6a218fd1b4
Jacob <104036951+j31c0@users.noreply.github.com>
2023-08-03 11:27:05 -0500

    Agregar funcionalidad a checklist

--- js-dom/script.js
+++ js-dom/script.js
@@ -24,8 +24,6 @@ const createTask = (evento) => {
     // task.innerHTML = content;
     task.appendChild(taskContent);
     list.appendChild(task);
-
-    console.log(content);
 };
 
 console.log(btn);
@@ -36,10 +34,14 @@ btn.addEventListener("click", createTask);
 const checkComplete = () => {
     const i = document.createElement("i");
     i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
 
     return i
 }
 
-const completeTask = () => {
-
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
 }
