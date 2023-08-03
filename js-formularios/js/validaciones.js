6b4ec16130f6f7b543b6aa16e999bf65d2cc2b13
Jacob <104036951+j31c0@users.noreply.github.com>
2023-08-03 14:24:10 -0500

    Agregar validaciones con modulos y modificar fecha

--- js-formularios/js/validaciones.js
+++ js-formularios/js/validaciones.js
@@ -1,8 +1,13 @@
-const inputNacimiento = document.querySelector("#birth");
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+}
 
-inputNacimiento.addEventListener("blur", (evento) => {
-    validarNacimiento(evento.target);
-})
+const validadores = {
+    nacimiento: (input) => validarNacimiento(input),
+}
 
 function validarNacimiento(input) {
     const fechaCliente = new Date(input.value);
