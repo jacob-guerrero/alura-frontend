a02570fde7284b6069b502ab4ac2d3a5d9930f24
Jacob <104036951+j31c0@users.noreply.github.com>
2023-08-04 21:12:50 -0500

    Agregar esqueleto crud y json-server

---
+++ js-crud/service/client-service.js
@@ -0,0 +1,13 @@
+const http = new XMLHttpRequest();
+
+//Abrir http (metodo,url)
+http.open("GET", "http://localhost:3000/perfil");
+
+http.send();
+
+http.onload = () => {
+    const data = http.response;
+    console.log(data);
+}
+
+console.log(http);
