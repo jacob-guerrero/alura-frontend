10f563146d5d63d39daaeb88cd86336b3f34eb61
Jacob <104036951+j31c0@users.noreply.github.com>
2023-08-08 18:18:21 -0500

    Finalizar proyecto actualizando informacion

--- js-crud-clientes/controllers/client.controller.js
+++ js-crud-clientes/controllers/client.controller.js
@@ -12,7 +12,7 @@ const crearNuevaLinea = (nombre, email, id) => {
       <ul class="table__button-control">
         <li>
           <a
-            href="../screens/editar_cliente.html"
+            href="../screens/editar_cliente.html?id=${id}"
             class="simple-button simple-button--edit"
           >
             Editar
@@ -32,11 +32,11 @@ const crearNuevaLinea = (nombre, email, id) => {
   btn.addEventListener("click", () => {
     const id = btn.id;
     clientServices
-    .eliminarCliente(id)
-    .then((respuesta) => {
-      console.log(respuesta);
-    })
-    .catch((err) => alert("Ocurrio un error"));
+      .eliminarCliente(id)
+      .then((respuesta) => {
+        console.log(respuesta);
+      })
+      .catch((err) => alert("Ocurrio un error"));
   });
 
   return linea;
@@ -47,7 +47,7 @@ const table = document.querySelector("[data-table]");
 clientServices
   .listaClientes()
   .then((data) => {
-    data.forEach(({nombre, email, id}) => {
+    data.forEach(({ nombre, email, id }) => {
       const nuevaLinea = crearNuevaLinea(nombre, email, id);
       table.appendChild(nuevaLinea);
     });
--- js-crud-clientes/service/client-service.js
+++ js-crud-clientes/service/client-service.js
@@ -7,18 +7,38 @@ const crearCliente = (nombre, email) => {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({nombre, email, id: uuid.v4()}),
+    body: JSON.stringify({ nombre, email, id: uuid.v4() }),
   });
 };
 
 const eliminarCliente = (id) => {
   return fetch(`http://localhost:3000/perfil/${id}`, {
-    method: "DELETE"
+    method: "DELETE",
+  });
+};
+
+const detalleCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`).then((respuesta) =>
+    respuesta.json()
+  );
+};
+
+const actualizarCliente = (nombre, email, id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre, email }),
   })
-}
+    .then((respuesta) => respuesta)
+    .catch((err) => console.log(err));
+};
 
 export const clientServices = {
   listaClientes,
   crearCliente,
   eliminarCliente,
+  detalleCliente,
+  actualizarCliente,
 };
