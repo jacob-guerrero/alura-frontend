094c61280356fa0fd6b9bef7c315c8e54eca1ab6
Jacob <104036951+j31c0@users.noreply.github.com>
2023-08-08 13:56:58 -0500

    Agregar funcionalidad de Eliminar clientes

--- js-crud-clientes/controllers/registro.controller.js
+++ js-crud-clientes/controllers/registro.controller.js
@@ -6,9 +6,10 @@ formulario.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
     const nombre = document.querySelector("[data-nombre]").value;
-    const email = document.querySelector("[data-email]").value
-    console.log(nombre, " - ", email);
-    clientServices.crearCliente(nombre, email).then(respuesta => {
+    const email = document.querySelector("[data-email]").value;
+    clientServices
+    .crearCliente(nombre, email)
+    .then(() => {
         window.location.href = "/screens/registro_completado.html";
     }).catch (err => console.log(err));
 })
--- js-crud-clientes/service/client-service.js
+++ js-crud-clientes/service/client-service.js
@@ -5,13 +5,20 @@ const crearCliente = (nombre, email) => {
   return fetch("http://localhost:3000/perfil", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({nombre, email, id: uuid.v4()})
+    body: JSON.stringify({nombre, email, id: uuid.v4()}),
+  });
+};
+
+const eliminarCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "DELETE"
   })
 }
 
 export const clientServices = {
   listaClientes,
   crearCliente,
+  eliminarCliente,
 };
