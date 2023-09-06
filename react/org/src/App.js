987841555f749921ad226db2a993041e8f3da6ce
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-06 10:48:27 -0500

    Agregar campos al formulario y estilos

--- react/org/src/App.js
+++ react/org/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './componentes/Header/Header.js';
+import Formulario from './componentes/Formulario/Formulario.js';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Formulario/>
     </div>
   );
 }
---
+++ react/org/src/componentes/CampoTexto/index.js
@@ -0,0 +1,10 @@
+import "./CampoTexto.css"
+
+const CampoTexto = () => {
+    return <div className="campo-texto">
+        <label>Nombre</label>
+        <input placeholder="Ingresar Nombre" />
+    </div>
+}
+
+export default CampoTexto
