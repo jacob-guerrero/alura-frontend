3dc9954dc9680829ca839ca97c9e6c21e756b2ce
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-07 12:51:11 -0500

    Agregar funcionalidad de mostrar/ocultar

--- react/org/src/App.js
+++ react/org/src/App.js
@@ -1,14 +1,25 @@
-import './App.css';
-import Header from './componentes/Header/Header.js';
-import Formulario from './componentes/Formulario/Formulario.js';
-import MiOrg from './componentes/MiOrg';
+import { useState } from "react";
+import "./App.css";
+import Header from "./componentes/Header/Header.js";
+import Formulario from "./componentes/Formulario/Formulario.js";
+import MiOrg from "./componentes/MiOrg";
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(true);
+
+  //Ternario -> condicion ? seMuestra : noSeMuestra
+  //Condicion && seMuestra
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario);
+  };
+
   return (
     <div>
-      <Header/>
-      <Formulario/>
-      <MiOrg/>
+      <Header />
+      {/* {mostrarFormulario ? <Formulario/> : <></>}; */}
+      {mostrarFormulario && <Formulario />}
+      <MiOrg cambiarMostrar={cambiarMostrar} />
     </div>
   );
 }
