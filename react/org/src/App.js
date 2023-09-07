736157a250ec885fe44b531c5a189225bd856e79
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-07 10:44:28 -0500

    Agregar seccion de organizacion con estilos

--- react/org/src/App.js
+++ react/org/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './componentes/Header/Header.js';
 import Formulario from './componentes/Formulario/Formulario.js';
+import MiOrg from './componentes/MiOrg';
 
 function App() {
   return (
     <div>
       <Header/>
       <Formulario/>
+      <MiOrg/>
     </div>
   );
 }
---
+++ react/org/src/componentes/MiOrg/index.js
@@ -0,0 +1,12 @@
+import "./MiOrg.css";
+
+const MiOrg = () => {
+  return (
+    <section className="orgSection">
+      <h3 className="title">Mi Organización</h3>
+      <img src="/img/add.png" alt="add" />
+    </section>
+  );
+};
+
+export default MiOrg;
