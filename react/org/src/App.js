c5c4e20a533042814ec5945e7506b00aca1e474e
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-08 12:59:01 -0500

    Agregar seccion equipo

--- react/org/src/App.js
+++ react/org/src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import Header from "./componentes/Header/Header.js";
 import Formulario from "./componentes/Formulario/Formulario.js";
 import MiOrg from "./componentes/MiOrg";
+import Equipo from "./componentes/Equipo";
 
 function App() {
-  const [mostrarFormulario, actualizarMostrar] = useState(true);
+  const [mostrarFormulario, actualizarMostrar] = useState(false);
 
   //Ternario -> condicion ? seMuestra : noSeMuestra
   //Condicion && seMuestra
@@ -20,6 +21,11 @@ function App() {
       {/* {mostrarFormulario ? <Formulario/> : <></>}; */}
       {mostrarFormulario && <Formulario />}
       <MiOrg cambiarMostrar={cambiarMostrar} />
+      <Equipo equipo="Programación" />
+      <Equipo equipo="Front End" />
+      <Equipo equipo="Data Science" />
+      <Equipo equipo="DevOps" />
+      <Equipo equipo="UX y Diseño" />
     </div>
   );
 }
