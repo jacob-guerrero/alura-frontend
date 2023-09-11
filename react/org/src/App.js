962f235c809cfe63095a4284a3e2b638eebd1dfa
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-11 11:49:32 -0500

    Mostrar colaboradores y filtrar equipos

--- react/org/src/App.js
+++ react/org/src/App.js
@@ -74,9 +74,13 @@ function App() {
       )}
       <MiOrg cambiarMostrar={cambiarMostrar} />
 
-      {equipos.map((equipo) => (
-        <Equipo datos={equipo} key={equipo.titulo} colaboradores={colaboradores} />
-      ))}
+      {
+        equipos.map((equipo) => (
+        <Equipo 
+        datos={equipo} 
+        key={equipo.titulo} 
+        colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)} />))
+      }
     </div>
   );
 }
--- react/org/src/componentes/Equipo/index.js
+++ react/org/src/componentes/Equipo/index.js
@@ -12,17 +12,23 @@ const Equipo = (props) => {
 
   const estiloTitulo = { borderColor: colorPrimario };
 
-  return (
+  return <>
+  {
+    colaboradores.length > 0 &&    
     <section className="equipo" style={obj}>
       <h3 style={estiloTitulo}>{titulo}</h3>
       <div className="colaboradores">
         {
           colaboradores.map((colaborador, index) => (
-          <Colaborador datos={colaborador} key={index}/>
-        ))}
+            <Colaborador
+            datos={colaborador}
+            key={index}
+            />
+            ))}
       </div>
     </section>
-  );
+  }
+  </>
 };
 
 export default Equipo;
