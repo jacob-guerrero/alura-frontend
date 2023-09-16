db0c601b7e6a17c1e64ec87796674829ae2ccc1f
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-15 20:55:46 -0500

    Agregar proyecto react router base

---
+++ react-router1/src/App.js
@@ -0,0 +1,26 @@
+import "./assets/css/base/base.css";
+import "./assets/css/componentes/card.css";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+
+function App() {
+
+  const location = window.location.pathname
+  console.log(location)
+
+  const Router = () => {
+    if(location === "/") {
+      return <Home />
+    } else {
+      return <Sobre />
+    }
+  }
+
+  return (
+    <>
+      {Router()}
+    </>
+  );
+}
+
+export default App;
