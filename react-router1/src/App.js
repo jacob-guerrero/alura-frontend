e0898ccf2b976e045e25e3c545b0bf1e402c9500
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-17 18:32:00 -0500

    Mostrar informacion de posts y manejar not found

--- react-router1/src/App.js
+++ react-router1/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 import Page404 from "./pages/Page404"
 import Header from "./components/Header";
+import Post from "./pages/Post";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -13,8 +14,9 @@ function App() {
     <Router>
       <Header />
        <Routes>
-        <Route  path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
+        <Route path="/posts/:id" element={<Post />} />
         <Route path="*" element={<Page404 />} />
        </Routes>
     </Router>
