efba2834a33034048a0643bd32c2b7eac5f132cc
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-19 13:37:42 -0500

    Agregar typography y switch

--- react-componentes-funcionales/src/components/FormSignUp.jsx
+++ react-componentes-funcionales/src/components/FormSignUp.jsx
@@ -1,16 +1,43 @@
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Switch from "@mui/material/Switch";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 function FormSignUp() {
   return (
     <form>
-      <TextField id="name" label="Nombre" variant="outlined" fullWidth margin="normal" />
-      <TextField id="latName" label="Apellidos" variant="outlined" fullWidth margin="normal" />
-      <TextField id="email" label="Email" variant="outlined" fullWidth margin="normal" />
-      <label>Promociones</label>
-      <input type="checkbox" />
-      <label>Novedades</label>
-      <input type="checkbox" />
+      <TextField
+        id="name"
+        label="Nombre"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="latName"
+        label="Apellidos"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="email"
+        label="Email"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch defaultChecked />}
+          label="Promociones"
+        />
+        <FormControlLabel
+          control={<Switch defaultChecked />}
+          label="Novedades"
+        />
+      </FormGroup>
       <Button variant="contained">Registrarse</Button>
     </form>
   );
