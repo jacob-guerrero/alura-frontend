33b2fb9426beae7abcf676790c7c9b4135342482
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-19 11:25:50 -0500

    Agregar proyecto base y componentes

---
+++ react-componentes-funcionales/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ClassComponent from './components/ClassComponent';
+import FuncComponent from './components/FuncComponent';
+
+function App() {
+  return (
+    <div>
+      <ClassComponent />
+      <FuncComponent />
+    </div>
+  );
+}
+
+export default App;
