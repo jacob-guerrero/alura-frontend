23ddf6d03232b6243bfc5bb1b17630f5abc83b7b
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-21 11:49:14 -0500

    Agregar proyecto base y modificar inputs

---
+++ react-hooks-context/src/Form/DatosUsuario/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { TextField, Button, Box } from "@mui/material";
+
+class DatosUsuario extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: {
+        value: "",
+        valid: true,
+      },
+      password: {
+        value: "",
+        valid: true,
+      },
+    };
+  }
+
+  render() {
+    return (
+      <Box
+        component="form"
+        autocomplete="off"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(this.state);
+        }}
+      >
+        <TextField
+          label="Correo electrónico"
+          variant="outlined"
+          fullWidth
+          margin="dense"
+          type="email"
+          error={false}
+          helperText={false && "Ingresa un correo electrónico válido"}
+          value={this.state.email.value}
+          onChange={(e) => {
+            this.setState({ email: { value: e.target.value } });
+          }}
+        />
+        <TextField
+          label="Contraseña"
+          variant="outlined"
+          fullWidth
+          margin="dense"
+          type="password"
+          value={this.state.password.value}
+          onChange={(e) => {
+            this.setState({ password: { value: e.target.value } });
+          }}
+        />
+        <Button variant="contained" type="submit">
+          Siguiente
+        </Button>
+      </Box>
+    );
+  }
+}
+
+export default DatosUsuario;
