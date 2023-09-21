2fddba24fdc105ac028b2763701c24a16ef4a2d4
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-21 14:21:20 -0500

    Agregar funcion para seleccionar form y stepper

--- react-hooks-context/src/Form/index.js
+++ react-hooks-context/src/Form/index.js
@@ -1,13 +1,33 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Typography,
+  useAutocomplete,
+  useScrollTrigger,
+} from "@mui/material";
 import { LogoSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
-/* import DatosPersonales from "./DatosPersonales";
+import { useState } from "react";
+import DatosPersonales from "./DatosPersonales";
 import DatosEntrega from "./DatosEntrega";
 import Complete from "./Complete";
-import Stepper from "../Stepper"; */
+import Stepper from "../Stepper";
 
 const Form = () => {
+  const [step, setStep] = useState(3);
+
+  // step 0 -> <DatosUsuario />
+  // step 1 -> <DatosPersonales />
+  // step 2 -> <DatosEntrega />
+  // step 3 -> <Complete />
+
+  const steps = {
+    0: <DatosUsuario />,
+    1: <DatosPersonales />,
+    2: <DatosEntrega />,
+    3: <Complete />,
+  };
+
   return (
     <Box
       sx={{
@@ -22,9 +42,11 @@ const Form = () => {
       </LogoSpace>
 
       <FormSpace>
-        <DatosUsuario />
-        {/* <DatosPersonales />
+        { step < 3 && <Stepper step={step} />}
+        {/* <DatosUsuario />
+        <DatosPersonales />
         <DatosEntrega /> */}
+        {steps[step]}
       </FormSpace>
     </Box>
   );
