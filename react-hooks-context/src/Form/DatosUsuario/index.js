3a01675c9d4afdf5fa32c66f69921594a586ad9a
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-21 12:24:24 -0500

    Agregar funciones para validar email y password

--- react-hooks-context/src/Form/DatosUsuario/index.js
+++ react-hooks-context/src/Form/DatosUsuario/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarEmail, validarPassword } from "./validaciones";
 
-class DatosUsuario extends React.Component {
-  constructor(props) {
+const DatosUsuario = () => {
+  const [email, setEmail] = useState({ value: "jacob@jacob.com", valid: true });
+  const [password, setPassword] = useState({ value: "abc", valid: true });
+
+  /* constructor(props) {
     super(props);
     this.state = {
       email: {
@@ -14,54 +18,55 @@ class DatosUsuario extends React.Component {
         valid: true,
       },
     };
-  }
+  } */
 
-  render() {
-    return (
-      <Box
-        component="form"
-        autocomplete="off"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
+  return (
+    <Box
+      component="form"
+      autocomplete="off"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(email, password);
+      }}
+    >
+      <TextField
+        label="Correo electrónico"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="email"
+        error={false}
+        helperText={false && "Ingresa un correo electrónico válido"}
+        value={email.value}
+        onChange={(e) => {
+          const email = e.target.value;
+          const valido = validarEmail(email);
+          setEmail({ value: email, valid: valido });
         }}
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log(this.state);
+      />
+      <TextField
+        label="Contraseña"
+        variant="outlined"
+        fullWidth
+        margin="dense"
+        type="password"
+        value={password.value}
+        onChange={(e) => {
+          const password = e.target.value;
+          setPassword({ value: password, valid: validarPassword(password) });
         }}
-      >
-        <TextField
-          label="Correo electrónico"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="email"
-          error={false}
-          helperText={false && "Ingresa un correo electrónico válido"}
-          value={this.state.email.value}
-          onChange={(e) => {
-            this.setState({ email: { value: e.target.value } });
-          }}
-        />
-        <TextField
-          label="Contraseña"
-          variant="outlined"
-          fullWidth
-          margin="dense"
-          type="password"
-          value={this.state.password.value}
-          onChange={(e) => {
-            this.setState({ password: { value: e.target.value } });
-          }}
-        />
-        <Button variant="contained" type="submit">
-          Siguiente
-        </Button>
-      </Box>
-    );
-  }
-}
+      />
+      <Button variant="contained" type="submit">
+        Siguiente
+      </Button>
+    </Box>
+  );
+};
 
 export default DatosUsuario;
