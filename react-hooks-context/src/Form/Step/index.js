458731c4e2615445e0ce0b2ba88c86b09e8c5ace
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-09-25 12:31:08 -0500

    Agregar custom Hooks

--- react-hooks-context/src/Form/Step/index.js
+++ react-hooks-context/src/Form/Step/index.js
@@ -1,12 +1,15 @@
 import React, { useState, useContext } from "react";
 import { TextField, Button, Box } from "@mui/material";
 import { CounterContext } from "../../Context";
+import useAuth from "../../Hooks/useAuth";
 
 const Step = ({ data, step }) => {
   const { inputs, buttonText, onSubmit } = data;
 
   const counterData = useContext(CounterContext);
-  console.log(counterData);
+
+  const access = useAuth("counterData.user.jwt");
+  console.log(access);
 
   return (
     <Box
@@ -20,7 +23,7 @@ const Step = ({ data, step }) => {
       }}
       onSubmit={onSubmit}
     >
-      <strong>El valor del contador es: {counterData.count} - {counterData.user.jwt}</strong>
+      <strong>El valor del contador es: {counterData.count}</strong>
 
       {inputs.map((input, i) => {
         const { label, type, value, valid, onChange, helperText, validator } =
