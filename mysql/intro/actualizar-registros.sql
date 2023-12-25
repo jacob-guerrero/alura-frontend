332750ea3045451e1f1671154563325032d63366
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-12-25 13:02:42 -0500

    Agregar sql para insertar y actualizar registros

---
+++ mysql/intro/actualizar-registros.sql
@@ -0,0 +1,5 @@
+UPDATE tbproductos SET precio = 28.51 WHERE producto = '695594';
+
+UPDATE tbproductos SET sabor = 'Lima/Limon', precio = 4.90 WHERE producto = '1041119';
+
+SELECT * FROM tbproductos;
