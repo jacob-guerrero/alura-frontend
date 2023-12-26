1c03e502c6656f11ed0f6fffd42d7c6799d9b0cd
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-12-26 10:19:27 -0500

    Agregar filtros

---
+++ mysql/intro/filtrar-registros.sql
@@ -0,0 +1,10 @@
+SELECT * FROM tbproducto;
+
+SELECT * FROM tbproducto WHERE SABOR = 'Maracuya';
+
+SELECT * FROM tbproducto WHERE ENVASE = 'Botella de vidrio';
+
+UPDATE tbproducto SET SABOR = 'citrico' WHERE SABOR = 'limon';
+
+SELECT * FROM tbproducto WHERE SABOR = 'Limon';
+SELECT * FROM tbproducto WHERE SABOR = 'Citrico';
